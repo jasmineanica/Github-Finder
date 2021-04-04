f0be6a64765bd8cbd17a1abd2c9acd4bb20249a2
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-04 15:15:39 -0700

    Navbar update
    - added default props to file
    - added proptypes for sanity checks

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  // default types when no props are passed in from App.js
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  };
+
+  //sets expected types and errors when not the expected type
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
 
   render() {
     return (
