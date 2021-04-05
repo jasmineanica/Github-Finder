fc4ef12d3890a0ef3bdae82ea4d6015a1eafc41c
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-04 17:14:32 -0700

    Updates
    - created OAuth with github
    - used global variables in .env.local for client id and client secret
    - created Search.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
@@ -23,7 +23,7 @@ const userStyle = {
   gridGap: '1rem'
 }
 
-Users.PropTypes = {
+Users.propTypes = {
   users: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired
 }
