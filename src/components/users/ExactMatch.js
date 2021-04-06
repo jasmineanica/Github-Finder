6d8b57c8a9f8bb33d83d1a4320d529e0637b5b56
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-06 15:08:56 -0700

    Refactored: ExactMatch to use useEffect hook

--- src/components/users/ExactMatch.js
+++ src/components/users/ExactMatch.js
@@ -1,100 +1,94 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
-export class ExactMatch extends Component {
-  static propTypes = {
-      loading: PropTypes.bool.isRequired,
-      user: PropTypes.object.isRequired,
-      showUser: PropTypes.bool.isRequired,
-      getUser: PropTypes.func.isRequired,
-  }
+const ExactMatch = ({ user, loading, showUser, getUser}) => {
 
-  componentDidMount() {
-    this.props.getUser(this.props.user.login);
-  }
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    company,
+    html_url,
+  } = user;
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      company,
-      html_url,
-    } = this.props.user;
+  useEffect(() => {
+    getUser(login);
+    // eslint-disable-next-line
+  }, []);
 
-    const { loading, showUser } = this.props;
-
-    if (loading) {
-      return <Spinner/>
-    } else {
-      return (
+  if (loading) {
+    return <Spinner/>
+  } else {
+    return (
+      <Fragment>
+      { showUser && (
         <Fragment>
-        { showUser && (
-          <Fragment>
-          <div className="headers">
-            <h2> Exact Match </h2>
+        <div className="headers">
+          <h2> Exact Match </h2>
+        </div>
+        <div className="card grid-2">
+          <div>
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
           </div>
-          <div className="card grid-2">
-            <div>
-              <img
-                src={avatar_url}
-                className="round-img"
-                alt=''
-                style={{ width: '150px' }}
-              />
-              <h1>{name}</h1>
-              <p>Location: {location}</p>
-            </div>
-            <div>
-              {bio &&
-                (<Fragment>
-                  <h3>Bio</h3>
-                  <p>{bio}</p>
-                </Fragment>)
-              }
-              <a href={html_url} className='btn btn-dark my-1'>
-                Visit Github Profile
-              </a>
-              <ul>
-                <li>
-                  {login && (
-                    <Fragment>
-                      <strong>Username: </strong>{login}
-                    </Fragment>
-                  )}
-                </li>
-                <li>
-                  {company && (
-                    <Fragment>
-                      <strong>Company: </strong>{company}
-                    </Fragment>
-                  )}
-                </li>
-                <li>
-                  {blog && (
-                    <Fragment>
-                      <strong>Website: </strong>{blog}
-                    </Fragment>
-                  )}
-                </li>
-              </ul>
-            </div>
+          <div>
+            {bio &&
+              (<Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>)
+            }
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>{login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>{company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>{blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
-          </Fragment>
-
-        )}
+        </div>
         </Fragment>
 
-      );
-    }
+      )}
+      </Fragment>
 
-
+    );
   }
 }
 
-
+ExactMatch.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    showUser: PropTypes.bool.isRequired,
+    getUser: PropTypes.func.isRequired,
+}
 export default ExactMatch;
