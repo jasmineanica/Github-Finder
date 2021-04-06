09a2ec61c7c3921123ec3a91e8c5a73da35e6fd8
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-05 19:08:56 -0700

    added user repo list

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -32,6 +33,13 @@ class App extends Component {
     this.setState({ user: res.data, loading: false});
   };
 
+  // Get user Repos
+  getUserRepos = async username => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ repos: res.data, loading: false});
+  }
+
 
   // Clear users from state
   clearUsers = () => {
@@ -46,7 +54,7 @@ class App extends Component {
   };
 
   render() {
-    const {users, user, loading, first} = this.state;
+    const {users, user, loading, repos} = this.state;
 
     return (
       <Router>
@@ -71,7 +79,9 @@ class App extends Component {
                 <User
                   {...props}
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
                   user={user}
+                  repos={repos}
                   loading={loading}
                 />
               )}/>
