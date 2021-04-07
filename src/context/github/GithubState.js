050079f31f1310a73e53ab2165306d494300e914
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-06 18:21:00 -0700

    moved Repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -43,13 +43,24 @@ const GithubState = props => {
     });
 
   };
+
    // Get Repo
+   const getUserRepos = async username => {
+     setLoading();
+     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+     dispatch({
+       type: GET_REPOS,
+       payload: res.data
+     });
+   }
+
    // Clear Users
    const clearUsers = () => {
      dispatch({
        type: CLEAR_USERS
      })
    };
+
    // Set loading
    const setLoading = () => {
      dispatch({ type: SET_LOADING });
@@ -63,7 +74,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
    >
     {props.children}
